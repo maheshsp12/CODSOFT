35fca8450ddeb94031f4786134aebea46e1b39be
maheshsp12 <144326090+maheshsp12@users.noreply.github.com>
2026-01-21 18:30:45 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,17 @@
+function append(value) {
+  document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function deleteLast() {
+  let current = document.getElementById("display").value;
+  document.getElementById("display").value = current.slice(0, -1);
+}
+
+function calculate() {
+  let result = eval(document.getElementById("display").value);
+  document.getElementById("display").value = result;
+}
